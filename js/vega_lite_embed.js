c6075d2039740e79c0e37de267daac8126d39f45
Chris <cwon0083@student.monash.edu>
2023-10-15 14:41:41 +1100

    Updating

---
+++ js/vega_lite_embed.js
@@ -0,0 +1,8 @@
+GDPmap = "js/GDPmap.vg.json"
+vegaEmbed("#GDPmap", GDPmap, {"actions": false});
+
+GDPbarchart = "js/gdpbarchart.vg.json"
+vegaEmbed("#gdpbarchart", GDPbarchart, {"actions": false});
+
+IQdotgraph = "js/iqdotgraph.vg.json"
+vegaEmbed("#iqdotgraph", IQdotgraph, {"actions": false});
--- js/vega_lite_vis.js
+++
@@ -1,2 +0,0 @@
-vg_1 = "js/GDPmap.vg.json"
-vegaEmbed("#GDPmap", vg_1);
