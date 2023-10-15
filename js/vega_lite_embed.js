d11110a8878f90e4e48c0bb9a36b86377a7d4515
Chris <cwon0083@student.monash.edu>
2023-10-15 21:34:28 +1100

    Finalising visualisation

--- js/vega_lite_embed.js
+++ js/vega_lite_embed.js
@@ -4,8 +4,17 @@ vegaEmbed("#GDPmap", GDPmap, {"actions": false});
 GDPbarchart = "js/gdpbarchart.vg.json"
 vegaEmbed("#gdpbarchart", GDPbarchart, {"actions": false});
 
+gdpbarchart2 = "js/gdpbarchart2.vg.json"
+vegaEmbed("#gdpbarchart2", gdpbarchart2, {"actions": false});
+
 IQdotgraph = "js/iqdotgraph.vg.json"
 vegaEmbed("#iqdotgraph", IQdotgraph, {"actions": false});
 
 gdphomicide = "js/gdphomicide.vg.json"
 vegaEmbed("#gdphomicide", gdphomicide, {"actions": false});
+
+donutchart1 = "js/donutchart1.vg.json"
+vegaEmbed("#donutchart1", donutchart1, {"actions": false});
+
+happinessScatterPlot = "js/happinessScatterPlot.vg.json"
+vegaEmbed("#happinessScatterPlot", happinessScatterPlot, {"actions": false});
